7ea11e047dd22cc5e07a5fca6fc1daec2e8a88fb
maksalexsavchenko <makskidexom@gmail.com>
2021-08-27 03:39:51 +0300

    homework 7 task 2

--- homework-07/src/js/task-02.js
+++ homework-07/src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsEl = document.getElementById('ingredients');
+
+function markUP (options) {
+    return options.map(option => {
+        let li = document.createElement('li');
+        li.textContent = option;
+        return li;
+    });
+}
+ingredientsEl.append(...markUP(ingredients));
